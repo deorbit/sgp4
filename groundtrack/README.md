b58ad05bee402cb0f6c09c3da7bc0ace9f1cd609
Adam Hollidge <adam@anh.io>
2014-02-13 12:47:57 -0500

    Regenereted aclocal.m4 (automake was producing 'version mismatch' error when generating groundtrack's Makefile.in and suggested re-running aclocal); stubbed out groundtrack tool.

---
+++ groundtrack/README.md
@@ -0,0 +1,3 @@
+# Groundtrack Generator
+
+	groundtrack [start datetime] [end datetime] [tle line 0] [tle line 1] [tle line 2]
---
+++ groundtrack/groundtrack.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2014 Adam Hollidge <adam@anh.io>
+ */
+
+ #include <SGP4.h>
+
+ #include <iostream>
+
+ int main()
+ {
+ 	std::cout << "This will generate ground tracks.\n";
+ 	return 0;
+ }
